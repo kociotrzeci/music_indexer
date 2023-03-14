99647f9ecd175a743fef048cd221c54cb85fc0b2
kociotrzeci <kociotrzeci@gmail.com>
2023-03-14 20:02:31 +0100

    switched to maven

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 import com.mpatric.mp3agic.InvalidDataException;
-import com.mpatric.mp3agic.Mp3File;
-import com.mpatric.mp3agic.ID3v2;
 import com.mpatric.mp3agic.UnsupportedTagException;
 import javax.swing.*;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
     String column[] = {"Directory", "Bitrate"};
@@ -11,29 +10,31 @@ public class Main {
     public static void main(String[] args) throws InvalidDataException, UnsupportedTagException, IOException {
         ScanFolder synth = new ScanFolder("SYNTHPOP");
         MusicFile[] listOfFiles = synth.getFileTable();
+        String[][] table = new String[listOfFiles.length][listOfFiles[0].getString().length];
         MusicDirectory[] listOfDirectories = synth.getDirectoryTable();
         System.out.println("------FILES-------");
-        for (MusicFile i : listOfFiles) {
-            i.print();
+        for (int i = 0; i < listOfFiles.length;i++) {
+            //listOfFiles[i].print();
+            table[i]= listOfFiles[i].getString();
+            System.out.println(Arrays.toString(table[i]));
         }
         System.out.println("------DIRECTORIES-------");
         for (MusicDirectory i : listOfDirectories) {
             System.out.println(i.getDirectory());
         }
-        String column[] = {"Name", "Roll Number", "Department"};
-        String[][] string = {
-                {"Kundan Kumar Jha", "4031"},
-                {"Anand Jha", "6014"}
-        };
-        /*
+        //new TableExample();
+
         JFrame window = new JFrame();
-        window.setContentPane(new Window().general);
+        Window pane = new Window();
+        window.setContentPane(pane.general);
         window.setVisible(true);
         window.setSize(1200, 600);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pane.setTable(table);
 
 
-         */
+
+
     }
     public static void passTable() {
         int i = 0;
--- src/Window.java
+++ src/Window.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import javax.swing.table.JTableHeader;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -25,5 +24,27 @@ public class Window {
         });
 
     }
+    public void setTable(String[][] table)
+    {
+        String[] heading = {"kk","ss","fa","dsd","sdfsd"};
+        this.table1 = new JTable(table, heading);
 
+/*
+        String data[][]={ {"101","Amit","670000"},
+                {"102","Jai","780000"},
+                {"101","Sachin","700000"}};
+        String column[]={"ID","NAME","SALARY"};
+        this.table1=new JTable(data,column);
+        this.table1.setBounds(30,40,200,300);
+
+ */
+
+
+
+
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
